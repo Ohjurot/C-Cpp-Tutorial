2c9874da162946275079561321ec66118a166bdf
Ludwig Füchsl <ludwig@fuechsl.org>
2022-07-31 15:19:35 +0200

    Variables

--- 01_SimpleCApp/SimpleCApp.c
+++ 01_SimpleCApp/SimpleCApp.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main()
 {
-	printf("Hello"); 
-	printf(" World!");
+	// (I) Simulate input (TODO: Replace with proper input)
+	int32_t x = 5;
+
+	// (P)
+	int32_t y = x * x + 5 * x + 5;
+
+	// (O)
+	printf("f(%i) = %i", x, y);
 }
