f5ffad125d4db12ba5023ee4bae85fdc7271c0e7
Ludwig Füchsl <ludwig@fuechsl.org>
2022-08-11 12:22:40 +0200

    Added first exercise

---
+++ Exercises/E01_Functions/Functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdint.h>
+
+/*
+ * Exercise 01 - Functions (e01)
+ * 
+ * ABOUT:
+ * Implementing sending over a serial interface.
+ * The given functions emulate a serial interface.
+ * 16-Bits are sended at a time. Each 16-Bit word is accompanied
+ * with a singel parity bit. From a give 16-Bit word the parity bit
+ * can be computed like this:
+ * 1 - Count the bits set in the 16-Bit data word
+ * 2 - Parity bit is 
+ *     0 - when count bits are even (0, 2, 4, 6, 8, 10, 12, 14, 16)
+ *     1 - when count bits are odd  (1, 3, 5, 7, 9, 11, 13, 15)
+ * The serial interface must be started (SerialBegin) and stoped (SerialEnd)
+ * correctly.
+ * 
+ * TODO:
+ * Send the following data via the Serial interface:
+ * 0xFFFF 0x1234 0x5678 0x9ABC 0xDEF0 0x0000
+ * Use a least one function to make the code more readable
+ * 
+ * HITS: 
+ *  - https://en.wikipedia.org/wiki/Euclidean_division
+ *  - https://en.wikipedia.org/wiki/Modulo_operation
+ */
+
+/*
+ *  Function given in e01
+ */
+uint16_t CountBits(uint16_t value);
+void SerialBegin();
+void SerialEnd();
+void SerialSend(uint16_t data, uint8_t parity);
+
+/*
+ *  Your function(s)
+ */
+
+/*
+ *  Implementation of main 
+ */
+int main()
+{
+
+}
+
+/*
+ *  Implementation of your function(s)
+ */
---
+++ Exercises/E01_Functions/e01.c
@@ -0,0 +1,74 @@
+/*
+ * 
+ *  Backend code for the 01 Functions exercise. 
+ *  Take a look at it and have fun ;-)
+ * 
+ *  NOTE: This will only work on windows
+ * 
+ */
+#include <stdint.h>
+#include <stdio.h>
+#include <intrin.h>
+
+int32_t serial_status = -1; // >= 0  - Read for data
+                            //  = -1 - Serial closed (not opened)
+                            //  = -2 - Serial closed (error)
+
+uint16_t CountBits(uint16_t value)
+{
+    return __popcnt16(value);
+}
+
+void SerialBegin()
+{
+    if (serial_status == -1)
+    {
+        printf(" === Ready for sending Serial Data === \n");
+        serial_status = 0;
+    }
+    else if (serial_status >= 0)
+    {
+        printf("Serial already opened!\n");
+    }
+    else
+    {
+        printf("Error in Serial interface!\n");
+    }
+}
+
+void SerialEnd()
+{
+    if (serial_status != -1)
+    {
+        printf(" === End of serial data transmition (Sent %i Bytes) === \n", serial_status);
+        serial_status = -1;
+    }
+    else
+    {
+        printf("Serial already closed!\n");
+    }
+}
+
+uint32_t SerialSend(uint16_t data, uint8_t parity)
+{
+    if (serial_status < 0)
+    {
+        printf("Error in Serial interface!\n");
+    }
+
+    if (parity != 0 && parity != 1)
+    {
+        serial_status = -2;
+    }
+
+    uint16_t bitCount = CountBits(data);
+    uint8_t expectedParity = bitCount % 2;
+    if (expectedParity != parity)
+    {
+        serial_status = -2;
+    }
+
+    printf("Sending: 0x%04X 0x%01X\n", data, parity);
+    serial_status++;
+}
+
