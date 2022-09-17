9ef0acdcbb9556300aad1be43edf90e1370a56a4
Ludwig Füchsl <ludwig@fuechsl.org>
2022-09-17 17:30:27 +0200

    if statement

--- 01_SimpleCApp/SimpleCApp.c
+++ 01_SimpleCApp/SimpleCApp.c
@@ -3,26 +3,25 @@
 
 int32_t f(int32_t x);
 int scan_x(int32_t* x);
+void execute_print(int32_t x);
 void print_fx(int32_t x, int32_t y);
 
 int main()
 {
-	// (I) 
-	int32_t x = 0;
+	int32_t x;
 	int result = scan_x(&x);
-	printf("Read %i parameters from user input. ", result);
-
-	// (P) 
-	int32_t y1 = f(1 * x);
-	int32_t y2 = f(2 * x);
-	int32_t y3 = f(3 * x);
-	int32_t y4 = f(4 * x);
-
-	// (O)
-	print_fx(1 * x, y1);
-	print_fx(2 * x, y2);
-	print_fx(3 * x, y3);
-	print_fx(4 * x, y4);
+	if (result == 1)
+	{
+		execute_print(x);
+	}
+	else if (result == EOF)
+	{
+		printf("IO Error!");
+	}
+	else
+	{
+		printf("The input must be a number!");
+	}
 }
 
 int32_t f(int32_t x)
@@ -34,6 +33,19 @@ int scan_x(int32_t* x)
 {
 	printf("Enter the value x: ");
 	return scanf_s("%i", x);
+}
+
+void execute_print(int32_t x)
+{
+	int32_t y1 = f(1 * x);
+	int32_t y2 = f(2 * x);
+	int32_t y3 = f(3 * x);
+	int32_t y4 = f(4 * x);
+
+	print_fx(1 * x, y1);
+	print_fx(2 * x, y2);
+	print_fx(3 * x, y3);
+	print_fx(4 * x, y4);
 }
 
 void print_fx(int32_t x, int32_t y)
