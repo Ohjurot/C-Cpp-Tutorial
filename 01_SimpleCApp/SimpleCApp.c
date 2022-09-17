cd9d3c3df2ecc70600f1508dca80a1537a2b66ad
Ludwig Füchsl <ludwig@fuechsl.org>
2022-09-17 16:48:51 +0200

    printf input

--- 01_SimpleCApp/SimpleCApp.c
+++ 01_SimpleCApp/SimpleCApp.c
@@ -2,12 +2,15 @@
 #include <stdint.h>
 
 int32_t f(int32_t x);
+int scan_x(int32_t* x);
 void print_fx(int32_t x, int32_t y);
 
 int main()
 {
-	// (I) Simulate input (TODO: Replace with proper input)
-	int32_t x = 5;
+	// (I) 
+	int32_t x = 0;
+	int result = scan_x(&x);
+	printf("Read %i parameters from user input. ", result);
 
 	// (P) 
 	int32_t y1 = f(1 * x);
@@ -25,6 +28,12 @@ int main()
 int32_t f(int32_t x)
 {
 	return x * x + 5 * x + 5;
+}
+
+int scan_x(int32_t* x)
+{
+	printf("Enter the value x: ");
+	return scanf_s("%i", x);
 }
 
 void print_fx(int32_t x, int32_t y)
