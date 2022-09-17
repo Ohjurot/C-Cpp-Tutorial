e95b2493e8b8124d7b66768a919439dc5ef3d408
Ludwig Füchsl <ludwig@fuechsl.org>
2022-09-17 18:00:01 +0200

    for loop (how often do you want your print and computations?)

--- 01_SimpleCApp/SimpleCApp.c
+++ 01_SimpleCApp/SimpleCApp.c
@@ -2,25 +2,30 @@
 #include <stdint.h>
 
 int32_t f(int32_t x);
-int scan_x(int32_t* x);
-void execute_print(int32_t x);
+int scan_int(int32_t* x);
+void execute_print(int32_t x, int32_t count);
 void print_fx(int32_t x, int32_t y);
 
 int main()
 {
-	int32_t x;
-	int result = scan_x(&x);
-	if (result == 1)
+	int32_t x, count;
+	int r0 = scan_int(&x);
+	int r1 = scan_int(&count);
+	if (r0 == 1 && r1 == 1 && count > 0)
 	{
-		execute_print(x);
+		execute_print(x, count);
 	}
-	else if (result == EOF)
+	else if (r0 == EOF || r1 == EOF)
 	{
 		printf("IO Error!");
 	}
+	else if (r1 == 1 && count <= 0)
+	{
+		printf("The count (second value) needs to be bigger than 0!");
+	}
 	else
 	{
-		printf("The input must be a number!");
+		printf("The input(s) must be a number!");
 	}
 }
 
@@ -29,23 +34,19 @@ int32_t f(int32_t x)
 	return x * x + 5 * x + 5;
 }
 
-int scan_x(int32_t* x)
+int scan_int(int32_t* x)
 {
-	printf("Enter the value x: ");
+	printf("Enter an integer value: ");
 	return scanf_s("%i", x);
 }
 
-void execute_print(int32_t x)
+void execute_print(int32_t x, int32_t count)
 {
-	int32_t y1 = f(1 * x);
-	int32_t y2 = f(2 * x);
-	int32_t y3 = f(3 * x);
-	int32_t y4 = f(4 * x);
-
-	print_fx(1 * x, y1);
-	print_fx(2 * x, y2);
-	print_fx(3 * x, y3);
-	print_fx(4 * x, y4);
+	for (int32_t i = 1; i <= count; i++)
+	{
+		int32_t y = f(i * x);
+		print_fx(i * x, y);
+	}
 }
 
 void print_fx(int32_t x, int32_t y)
